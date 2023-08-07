90be3723bdb25d6c4dfeed609468cfee7e982992
Aidenbuis <aidenbuis@gmail.com>
2023-08-07 13:28:30 +0200

    typo

--- src/pages/flex/4.tsx
+++ src/pages/flex/4.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Exercise2 = () => {
   return (
     <ExerciseLayout>
-      <h2>💪 The Flexible Box Layout: Exercise 3</h2>
+      <h2>💪 The Flexible Box Layout: Exercise 4</h2>
       <p>In this exercise you will create a multiline flexbox layout.</p>
       <h3>Current Code</h3>
       <div className="flex-ex-3-container">
